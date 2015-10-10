25cddd3d2debc425d26814287b3947f4db845562
Vivekanandan, Karthik (kvivek) <kvivekanandan@stubhub.com>
2015-10-10 15:50:12 -0700

    crawled 1000 nodes

--- graph.py
+++ graph.py
@@ -38,11 +38,13 @@ def print_degree_sequences(G):
         if i not in seen:
             seen.add(i)
 
-    for i in sorted(seen, reverse=True):
+    for i in sorted(seen, reverse=False):
         print str(i) + " : " + str(degree_sequence.count(i))
+        dict.update({str(i):str(degree_sequence.count(i))})
 
     size = len(seen)
     print("unique degrees in graph: " + str(size))
+    return dict
 
 def print_in_degree_sequences(G):
     degree_sequence=sorted(G.in_degree().values(),reverse=True) # degree sequence
@@ -79,6 +81,22 @@ def print_centralities(G, nodes, dc, eigc, pgc):
     for node in nodes:
         print "{:<15} {:<8} {:<20} {:<20} {:<20}".format(str(node),str(G.in_degree(node)),str(dc[node]),str(eigc[node]),str(pgc[node]))
 
+def three_cycles(G):
+    cycles = nx.simple_cycles(G)
+    for c in cycles:
+        if len(c)==3:
+            print c
+    return cycles
+
+def calculate_largest_connected_component():
+    pass
+
+def print_list(list):
+    n =0
+    for i in list:
+        print ((str(n)) + " " + str(i))
+        n = n + 1
+
 #main
 
 lines = readEdgeListFromFile()
@@ -88,15 +106,21 @@ edges = G.edges()
 print "num of nodes from parsing edgeList: " + str(len(nodes))
 print "num of edges from parsing edgeList: " + str(len(edges))
 
+degree_histogram = nx.degree_histogram(G)
+print "degree histogram: "
+print_list(degree_histogram)
+dict = print_degree_sequences(G)
+print_dictionary(dict)
 #print_degree_sequences(G)
 #convert to gigraph
 directed_G = nx.convert.convert_to_directed(G)
 #print_in_degree_sequences(directed_G)
 
-degreeCentrality = degree_centrality(G)
-eigenVectorCentrality = eigenvector_centrality(G)
-pageRankCentrality = pagerank_centrality(G)
-print_centralities(directed_G, nodes,degreeCentrality,eigenVectorCentrality,pageRankCentrality)
+#degreeCentrality = degree_centrality(G)
+#eigenVectorCentrality = eigenvector_centrality(G)
+#pageRankCentrality = pagerank_centrality(G)
+#print_centralities(directed_G, nodes,degreeCentrality,eigenVectorCentrality,pageRankCentrality)
+#three_cycles(directed_G)
 
 #visualize graph
 #nx.draw_spring(directed_G, node_size=0,edge_color='b',alpha=0.2,font_size=10,with_labels=True)
