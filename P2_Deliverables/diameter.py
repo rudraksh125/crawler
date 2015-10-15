cb150b0573fd2ed375842366af123346bc17801e
Harshaneel Gokhale <harshaneelhg@gmail.com>
2015-10-14 20:47:47 -0700

    Update diameter.py

--- P2_Deliverables/diameter.py
+++ P2_Deliverables/diameter.py
@@ -1,5 +1,13 @@
 import networkx as nx
-
+import snap
+"""
 new_G = nx.read_graphml('networkx_graph.graphml')
 G = new_G.to_undirected()
 print "diameter of the graph :" + str(nx.diameter(G))
+"""
+
+G = snap.TNGraph.Load(snap.TFIn("../test.graph"))
+test_nodes = 500
+
+diameter = snap.GetBfsFullDiam(G,test_nodes,True)
+print "Diameter of the network= %f" % (diameter)
