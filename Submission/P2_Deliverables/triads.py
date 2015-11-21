ff6f14dcd86fe40d654e03d64edf9e706449d1a0
Vivekanandan, Karthik (kvivek) <kvivekanandan@stubhub.com>
2015-11-21 12:32:23 -0700

    reorg

--- Submission/P2_Deliverables/triads.py
+++
@@ -1,6 +0,0 @@
-import networkx as nx
-
-new_G = nx.read_graphml('networkx_graph.graphml')
-undirected_G = new_G.to_undirected()
-num_triads = nx.triangles(undirected_G)
-print "triads " + str(len(list(num_triads.values())))
